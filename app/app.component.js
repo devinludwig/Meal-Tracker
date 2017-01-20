4312432244251b459672b9685944af9d50708d8a
Devin Ludwig <devinludwig@gmail.com>
2017-01-20 09:14:08 -0800

    fill out app.component with notation and declaration, including selector tags for subcomponents and appropriate methods for food editing and creation.

--- app/app.component.js
+++ app/app.component.js
@@ -8,13 +8,28 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.currentTime = new Date;
+        this.month = this.currentTime.getMonth() + 1;
+        this.day = this.currentTime.getDate();
+        this.year = this.currentTime.getFullYear();
+        this.selectedFood = null;
+        this.masterFoodList = [];
     }
+    AppComponent.prototype.addFood = function (newFood) {
+        this.masterFoodList.push(newFood);
+    };
+    AppComponent.prototype.editFood = function (clickedFood) {
+        this.selectedFood = clickedFood;
+    };
+    AppComponent.prototype.finishedEditing = function () {
+        this.selectedFood = null;
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'app-root',
-        template: "\n  <div class=\"container\">\n  <h1>My First Angular 2 App</h1>\n  </div>\n  "
+        template: "\n  <div class=\"container\">\n    <h1>Calorie Conductor</h1>\n    <h3>Your Diet Assistant</h3>\n    <food-list [childFoodList]=\"masterFoodList\" (clickSender)=\"editFood($event)\"></food-list>\n    <hr>\n    <edit-food [childSelectedFood]=\"selectedFood\" (doneButtonClickedSender)=\"finishedEditing()\"></edit-food>\n    <new-food (newFoodSender)=\"addFood($event)\"></new-food>\n  </div>\n  "
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,11 +5,34 @@ import { Food } from './food.model';
   selector: 'app-root',
   template: `
   <div class="container">
-  <h1>My First Angular 2 App</h1>
+    <h1>Calorie Conductor</h1>
+    <h3>Your Diet Assistant</h3>
+    <food-list [childFoodList]="masterFoodList" (clickSender)="editFood($event)"></food-list>
+    <hr>
+    <edit-food [childSelectedFood]="selectedFood" (doneButtonClickedSender)="finishedEditing()"></edit-food>
+    <new-food (newFoodSender)="addFood($event)"></new-food>
   </div>
   `
 })
 
 export class AppComponent {
+  currentTime = new Date;
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  selectedFood = null;
 
+  masterFoodList: Food[] = [];
+
+  addFood(newFood: Food) {
+    this.masterFoodList.push(newFood);
+  }
+
+  editFood(clickedFood) {
+    this.selectedFood = clickedFood;
+  }
+
+  finishedEditing() {
+    this.selectedFood = null;
+  }
 }
